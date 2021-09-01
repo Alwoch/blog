173baf08d4c0dc0d21367b40b87b0fed4b2bf29f
Alwoch <salwoch@gmail.com>
2021-09-01 12:03:55 +0300

    added a new blog

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Create from "./create";
+import BlogDetails from "./blog-details";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
